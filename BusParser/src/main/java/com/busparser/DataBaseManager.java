10fbe9956a0692043558175683b529fdd0fdcc3e
wblachowski <wojciech@blachowski.pl>
2018-02-16 17:09:30 +0100

    Inserting buses into db

--- BusParser/src/main/java/com/busparser/DataBaseManager.java
+++ BusParser/src/main/java/com/busparser/DataBaseManager.java
@@ -27,15 +27,35 @@ public class DataBaseManager {
     }
 
     public void insertBus(String busName) {
-
+        int region = resolveRegion(busName);
+        String sql="INSERT INTO buses VALUES (?,?)";
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, busName);
+            pstmt.setInt(2, region);
+            pstmt.executeUpdate();
+            System.out.println("Bus " + busName + " inserted");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void insertStop(String url, String stopName) {
 
     }
 
+    private void createTable(String sql) {
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     private void initalizeTables() {
         initalizeRegions();
+        initalizeBuses();
     }
 
     private void initalizeRegions() {
@@ -43,29 +63,39 @@ public class DataBaseManager {
                 + "	id integer PRIMARY KEY,\n"
                 + "	name text NOT NULL\n"
                 + ");";
-        try (Connection conn = DriverManager.getConnection(url);
-             Statement stmt = conn.createStatement()) {
-            stmt.execute(sql);
-            populateRegions();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        createTable(sql);
+        populateRegions();
     }
 
-    private void populateRegions(){
-        String sql="INSERT INTO regions(id,name) VALUES(?,?)";
-        int[] ids={0,1,2};
-        String[] names={"Linie do Poznania","Linie na terenie Gminy Swarzędz","Linie międzygminne"};
-        for(int i=0;i<ids.length;i++){
+    private void initalizeBuses() {
+        String sql = "CREATE TABLE IF NOT EXISTS buses (\n"
+                + "	name TEXT PRIMARY KEY,\n"
+                + "	region_id INTEGER,\n"
+                +" FOREIGN KEY(region_id) REFERENCES regions(id)\n"
+                + ");";
+        createTable(sql);
+    }
+
+    private void populateRegions() {
+        String sql = "INSERT INTO regions(id,name) VALUES(?,?)";
+        int[] ids = {0, 1, 2};
+        String[] names = {"Linie do Poznania", "Linie na terenie Gminy Swarzędz", "Linie międzygminne"};
+        for (int i = 0; i < ids.length; i++) {
             try (Connection conn = DriverManager.getConnection(url);
                  PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                pstmt.setInt(1,ids[i]);
+                pstmt.setInt(1, ids[i]);
                 pstmt.setString(2, names[i]);
                 pstmt.executeUpdate();
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private int resolveRegion(String busname){
+        if(busname.startsWith("49"))return 1;
+        if(busname.startsWith("48"))return 2;
+        return 0;
     }
 
     private void loadProperties() {
