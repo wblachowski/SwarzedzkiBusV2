98a8245be6fab7821b68e4e8025fa371b4c08bc5
wblachowski <wojciech@blachowski.pl>
2018-02-18 15:46:16 +0100

    Added textField in search tab

--- SwarzedzkiBus/app/src/main/java/com/wblachowski/swarzedzkibus/fragments/SearchFragment.java
+++ SwarzedzkiBus/app/src/main/java/com/wblachowski/swarzedzkibus/fragments/SearchFragment.java
@@ -1,11 +1,10 @@
 package com.wblachowski.swarzedzkibus.fragments;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.wblachowski.swarzedzkibus.R;
 
@@ -20,8 +19,6 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText("SZUKAJ");
         return rootView;
     }
 }
