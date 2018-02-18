01358c634307139ec23f0d19562ffab9a5cc5c65
wblachowski <wojciech@blachowski.pl>
2018-02-18 15:19:02 +0100

    Added tab selection

--- SwarzedzkiBus/app/src/main/java/com/wblachowski/swarzedzkibus/fragments/SearchFragment.java
+++ SwarzedzkiBus/app/src/main/java/com/wblachowski/swarzedzkibus/fragments/SearchFragment.java
@@ -1,10 +1,27 @@
 package com.wblachowski.swarzedzkibus.fragments;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wblachowski.swarzedzkibus.R;
 
 /**
  * Created by wblachowski on 2/18/2018.
  */
 
 public class SearchFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("SZUKAJ");
+        return rootView;
+    }
 }
